d72b6c883720578027f2117e1d8b592fe7e8bd5c
Andrey Arzamaskov <arzamaskov@gmail.com>
2026-05-10 10:25:54 +0400

    test: rename example unit test

--- tests/Unit/ApplicationNameTest.php
+++ tests/Unit/ApplicationNameTest.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+class ApplicationNameTest extends TestCase
 {
     public function test_application_name_can_be_normalized(): void
     {
